83d1d24d4ec396f228d2c2e1a21b2b446d38e786
Victor Hevia Martin <rcpoetics@gmail.com>
2024-02-11 11:04:47 +0100

    Ultimos cambios no consolidados.

--- src/scrapper.py
+++ src/scrapper.py
@@ -5,8 +5,8 @@ import wget
 import os
 
 head = {
-        "User-Agent": "Mozilla/5.0 (X11; U; Linux amd64; rv:5.0) Gecko/20100101 Firefox/5.0 (Debian)",
-        "X-Requested-With": "XMLHttpRequest"
+        "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201"#,
+        #"X-Requested-With": "XMLHttpRequest"
 }
 
 url = "https://datos.madrid.es/portal/site/egob/menuitem.c05c1f754a33a9fbe4b2e4b284f1a5a0/?vgnextoid=aecb88a7e2b73410VgnVCM2000000c205a0aRCRD&vgnextchannel=374512b9ace9f310VgnVCM100000171f5a0aRCRD"
